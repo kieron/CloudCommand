b8758ce17bf611426d513f1886add19f01dd2e08
phunkb <kieronboz@gmail.com>
2019-09-14 19:37:35 +0100

    Create QuickCommands.sh

---
+++ QuickCommands.sh
@@ -0,0 +1,60 @@
+
+#!/bin/bash
+# Cloudbox Quick Command Launcher
+echo "Cloudbox Quick Command Launcher"
+echo
+PS3='Choose Command: '
+options=("Plex AutoScan Log" "Cloudplow Log" "NCDU /opt (excluding Plex)" "NCDU /opt (including Plex)" "Show Certificate Information" "Renew Necessary Certi$
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Plex AutoScan Log")
+        echo "Launching Plex Autoscan Log Tail"
+        echo
+        tail -f /opt/plex_autoscan/plex_autoscan.log -n 30
+        ;;
+        "Cloudplow Log")
+        echo "Launching Cloudplow Log Tail"
+        echo
+        tail -f /opt/cloudplow/cloudplow.log -n 30
+        ;;
+        "NCDU /opt (excluding Plex)")
+        echo "Launching NCDU (excluding Plex)"
+        echo
+        ncdu -x /opt --exclude=/opt/plex
+        ;;
+        "NCDU /opt (including Plex)")
+        echo "Launching NCDU (including Plex)"
+        ncdu /opt
+        ;;
+        "NCDU Local Mount")
+        echo "Launching NCDU in Local Mount"
+        ncdu "/mnt/local/"
+        ;;
+        "Get Plex Token")
+        echo "Launching Plex Token Script"
+        /opt/scripts/plex/plex_token.sh
+        ;;
+        "Show Certificate Information")
+        echo "Launching Certificate Information"
+        docker exec letsencrypt /app/cert_status
+        ;;
+        "Renew Necessary Certificates")
+        echo "Forcing Renew of all Necessary Cerificates"
+        docker exec letsencrypt /app/signal_le_service
+        ;;
+        "Force Renew ALL Certificates")
+        echo "Forcing Renew of ALL Cerificates"
+        docker exec letsencrypt /app/cert_status
+        ;;
+        "Nench Benchmark")
+        echo "Launching Nench Benchmark"
+        echo
+        curl -s wget.racing/nench.sh | bash
+        ;;
+        "Quit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
