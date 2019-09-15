45a57d5702013124d5410f7152ef09d2fde1ab0c
phunkb <kieronboz@gmail.com>
2019-09-15 19:17:28 +0100

    added figlet

--- QuickCommands.sh
+++ QuickCommands.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 # Cloudbox Quick Command Launcher v.0.1
 figlet CloudCommand
-echo -e "\e[32mCloudbox Quick Command Launcher\e[37m"
-PS3='Choose Command: '
+echo -e "\e[32mCloudbox Quick Command Launcher"
+echo -e "Choose a command from below:\e[37m"
+echo
+PS3='Enter Command: '
 options=("Plex AutoScan Log" "Cloudplow Log" "NCDU /opt (excluding Plex)" "NCDU /opt (including Plex)" "Plex Sync Folder Size" "Show Certificate Information" "Fix Plex Trash" "Renew Necessary Certificates" "Force Renew ALL Certificates" "NCDU Local Mount" "Get Plex Token" "Nench Benchmark" "Quit")
 select opt in "${options[@]}"
 do
